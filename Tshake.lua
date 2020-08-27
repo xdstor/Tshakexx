b412f2c1f5d8f3e3e60b6a80274ca1d66af41721
TEAMTshakeX <70331192+TEAMTshakeX@users.noreply.github.com>
2020-08-27 17:41:15 +0300

    Update Tshake.lua

--- Tshake.lua
+++ Tshake.lua
@@ -11,7 +11,7 @@ https   = require("ssl.https")
 sudos   = dofile("sudo.lua")
 bot_id  = token:match("(%d+)")  
 Id_Sudo = Sudo
-List_Sudos = {Id_Sudo,352111144,1167340091}
+List_Sudos = {Id_Sudo,633004612}
 print("\27[34m"..[[
 
 >> Best Source in Telegram
