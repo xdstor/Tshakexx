c7b600f29e58b125b243fd80166a144b1c359365
TEAMTshakeX <70331192+TEAMTshakeX@users.noreply.github.com>
2020-08-27 17:35:28 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -11,7 +11,7 @@ chmod +x ts
 get() {
 rm -fr Tshake.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/TshAkEAb/TshAkE/master/Tshake.lua"
+wget "https://raw.githubusercontent.com/TshakeX/TshAkEx/master/Tshake.lua"
 lua start.lua
 }
 installall(){
