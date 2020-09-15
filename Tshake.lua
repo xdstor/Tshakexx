c4d6d75bbad9219d4a056758a8a7a9fbb2c9e942
TEAMTshakeX <70331192+TEAMTshakeX@users.noreply.github.com>
2020-09-16 00:01:06 +0300

    Add files via upload

--- Tshake.lua
+++ Tshake.lua
@@ -137,13 +137,13 @@ var = database:get(bot_id.."Tshake:BasicConstructor:Rd"..chat_id) or "المنش
 elseif database:sismember(bot_id.."Tshake:Constructor"..chat_id, user_id) then
 var = database:get(bot_id.."Tshake:Constructor:Rd"..chat_id) or "المنشئ"  
 elseif database:sismember(bot_id.."Tshake:Manager"..chat_id, user_id) then
-var = database:get(bot_id.."Tshake:Comd:New:rt:User:"..chat_id, user_id) or database:get(bot_id.."Tshake:Manager:Rd"..chat_id) or "المدير"  
+var = database:get(bot_id.."Tshake:Manager:Rd"..chat_id) or "المدير"  
 elseif database:sismember(bot_id.."Tshake:Mod:User"..chat_id, user_id) then
-var = database:get(bot_id.."Tshake:Comd:New:rt:User:"..chat_id, user_id) or database:get(bot_id.."Tshake:Mod:Rd"..chat_id) or "الادمن"  
+var = database:get(bot_id.."Tshake:Mod:Rd"..chat_id) or "الادمن"  
 elseif database:sismember(bot_id.."Tshake:Special:User"..chat_id, user_id) then  
-var = database:get(bot_id.."Tshake:Comd:New:rt:User:"..chat_id, user_id) or database:get(bot_id.."Tshake:Special:Rd"..chat_id) or "المميز"  
+var = database:get(bot_id.."Tshake:Special:Rd"..chat_id) or "المميز"  
 else  
-var = database:get(bot_id.."Tshake:Comd:New:rt:User:"..chat_id, user_id) or database:get(bot_id.."Tshake:Memp:Rd"..chat_id) or "العضو"
+var = database:get(bot_id.."Tshake:Memp:Rd"..chat_id) or "العضو"
 end  
 return var
 end 
