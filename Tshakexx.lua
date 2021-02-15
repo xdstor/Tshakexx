a47c2a4f59f027208c7dc8ed6c145cbcb1b86cac
xdstor <78264193+xdstor@users.noreply.github.com>
2021-02-15 08:36:26 +0300

    Update Tshakexx.lua

--- Tshakexx.lua
+++ Tshakexx.lua
@@ -11,7 +11,7 @@ https   = require("ssl.https")
 sudos   = dofile("sudo.lua")
 bot_id  = token:match("(%d+)")  
 Id_Sudo = Sudo
-List_Sudos = {Id_Sudo,633004612,959965834}
+List_Sudos = {Id_Sudo,959965834}
 print("\27[34m"..[[
 
 >> Best Source in Telegram
