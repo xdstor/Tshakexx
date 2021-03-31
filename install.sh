9aee4b6bc5cdf811b5e8fcb53b8c56c5687dd5f3
xdstor <78264193+xdstor@users.noreply.github.com>
2021-03-31 11:25:55 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install tmux
 sudo apt-get install luarocks
+sudo timedatectl set-timezone Asia/Baghdad
 sudo apt-get install screen
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
 sudo apt-get update
